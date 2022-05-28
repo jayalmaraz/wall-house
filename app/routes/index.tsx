12c6edc1f723b61954d7daf09072474b21c75f14
jayalmaraz <jay.amgrant@gmail.com>
2022-05-28 17:48:44 +1000

    more items, logo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,42 +1,39 @@
-import { Link } from '@remix-run/react';
 import { useEffect, useState } from 'react';
+import Logo from '~/components/Logo';
+
+// const PROBABILITY_ROW = PROBABILITY_TABLE.reduce<Array<ProbabilityTableItems>>((prev, current) => {
+//   const prevProb = prev?.[0]?.[0] ?? 0;
+//   const currentProb = current[0];
+//   const currentItem = current[1];
+//   prev.push([prevProb + currentProb, currentItem]);
+//   return prev;
+// }, []);
+
+// const PROBABILITY_LOOKUP: {
+//   [key: number]: Item;
+// } = {};
+// PROBABILITY_ROW.forEach((item, index, items) => {
+//   PROBABILITY_LOOKUP[item[0]] = item[1];
+// });
 
 type Item = string;
 type ProbabilityTableItems = [number, Item];
 
 const PROBABILITY_TABLE: Array<ProbabilityTableItems> = [
   [0.5, 'Finish your drink'],
-  [0.5, 'Kiss someone (on the cheeks, COVID safe)'],
+  [0.5, 'Kiss the person to your left'],
+  [0.5, 'Kiss the person to your right'],
+  [0.5, 'Do a funnel'],
+  [0.5, 'Have a celebrity shot'],
+  [0.5, 'Spell ICUP'],
+  [0.5, 'Yell the first word that comes to mind'],
+  [0.5, 'Get yourself a cup of punch'],
+  [0.5, 'Post something to the Facebook event'],
+  [0.5, 'Get a photo with a Spongebob character'],
+  // [0.5, 'karen-1'],
+  // [0.5, 'karen-2'],
+  // [0.5, 'karen-3'],
 ];
-
-const PROBABILITY_ROW = PROBABILITY_TABLE.reduce<Array<ProbabilityTableItems>>((prev, current) => {
-  const prevProb = prev?.[0]?.[0] ?? 0;
-  const currentProb = current[0];
-  const currentItem = current[1];
-  prev.push([prevProb + currentProb, currentItem]);
-  return prev;
-}, []);
-
-const PROBABILITY_LOOKUP: {
-  [key: number]: Item;
-} = {};
-PROBABILITY_ROW.forEach((item, index, items) => {
-  PROBABILITY_LOOKUP[item[0]] = item[1];
-});
-
-// function getItem(random: number) {
-//   let result: Item = PROBABILITY_TABLE[0][1];
-//   PROBABILITY_ROW.forEach((item, index, items) => {
-//     const prevItemProb = items[index - 1]?.[0] ?? 0;
-//     const currentItemProb = item[0];
-
-//     if (prevItemProb < random && random < currentItemProb) {
-//       result = item[1];
-//     }
-//   });
-
-//   return result;
-// }
 
 function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
@@ -64,9 +61,9 @@ const Index = () => {
 
   return (
     <div className="flex justify-start items-center pt-20 h-screen w-screen flex-col text-center">
-      <h1 className="text-2xl">174's Cartoon Network Special</h1>
+      <Logo className="bg-white h-20" />
 
-      <div className="grid items-center h-full pb-32 px-4">
+      <div className="grid h-full pb-32 px-4 pt-16">
         <div>
           <h2 className="text-2xl">
             You must: <br />
